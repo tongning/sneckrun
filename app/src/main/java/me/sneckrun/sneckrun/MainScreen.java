08c1e0a0061c0ef867754fe9af09be8b26b65c18
Anthony Li <antli.inbox@gmail.com>
2017-03-11 22:22:43 -0500

    Add toast

--- app/src/main/java/me/sneckrun/sneckrun/MainScreen.java
+++ app/src/main/java/me/sneckrun/sneckrun/MainScreen.java
@@ -5,13 +5,23 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 public class MainScreen extends AppCompatActivity {
-
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
+        button = (Button) findViewById(R.id.needSneck);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "You requested a sneck run!",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 
     public void initiateSneckRun(View view){
