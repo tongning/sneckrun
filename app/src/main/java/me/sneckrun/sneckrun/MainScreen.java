5882eb10e904fbca6dc4dee7a8586395ba836dd2
Anthony Li <antli.inbox@gmail.com>
2017-03-12 00:44:02 -0500

    Make http requests

--- app/src/main/java/me/sneckrun/sneckrun/MainScreen.java
+++ app/src/main/java/me/sneckrun/sneckrun/MainScreen.java
@@ -3,6 +3,7 @@ package me.sneckrun.sneckrun;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.icu.text.RelativeDateTimeFormatter;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -11,9 +12,16 @@ import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 import android.widget.EditText;
+import java.util.*;
+import org.json.*;
+import java.io.*;
+import okhttp3.*;
+import android.util.Log;
+
 //import android.widget.Context;
 
 import com.google.firebase.messaging.FirebaseMessaging;
+
 
 public class MainScreen extends AppCompatActivity {
     private Button button;
@@ -22,6 +30,7 @@ public class MainScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         FirebaseMessaging.getInstance().subscribeToTopic("aces-sneck-run");
 
+        final Handler mHandler = new Handler(getMainLooper());
 
         setContentView(R.layout.activity_main_screen);
         button = (Button) findViewById(R.id.needSneck);
@@ -29,8 +38,47 @@ public class MainScreen extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "You requested a sneck run!",
-                        Toast.LENGTH_SHORT).show();
+
+                MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("name", "some name");
+
+                JSONObject parameter = new JSONObject(params);
+                OkHttpClient client = new OkHttpClient();
+
+                RequestBody body = RequestBody.create(JSON, parameter.toString());
+                Request request = new Request.Builder()
+                        .url("http://snackrun.herokuapp.com/message")
+                        .post(body)
+                        .addHeader("content-type", "application/json; charset=utf-8")
+                        .build();
+
+                client.newCall(request).enqueue(new Callback() {
+                    @Override
+                    public void onFailure(Call call, IOException e) {
+
+                        mHandler.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                Toast.makeText(getApplicationContext(), "Request failed.", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+
+
+                    }
+
+                    @Override
+                    public void onResponse(Call call, Response response) throws IOException {
+                        mHandler.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                Toast.makeText(getApplicationContext(), "You requested a sneck run!", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+
+
+                });
             }
         });
 
